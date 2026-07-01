d84fc538cc8f56d875b5ff680d381ee8d60d4f31
ANIELSUNN <166258290+ANIELSUNN@users.noreply.github.com>
2026-07-01 20:50:15 +0200

    Fix jwt-decode named export import

--- src/pages/SignatureWorkspace.jsx
+++ src/pages/SignatureWorkspace.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { decode as jwtDecode } from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 import axios from 'axios';
 
